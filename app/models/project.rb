0fd8be310fb33bddc39227ef665e4accda3b5181
Nick Waggoner <nickwaggonersw@gmail.com>
2021-11-10 21:33:36 +0000

    circleci

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
     #Need to require the title and description of the project
-    #validates_presence_of :title, :description
+    validates_presence_of :title, :description
 end
