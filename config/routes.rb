477e9aabc23c538554e20a38eaff0f47889a783f
Nick Waggoner <nickwaggonersw@gmail.com>
2021-12-01 01:02:11 +0000

    Devise views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "projects#index"
   resources :projects
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :users, skip: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
